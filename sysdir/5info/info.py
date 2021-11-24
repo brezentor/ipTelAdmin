15dd4bbe23e2f80e80b176828e8541f9854dcc0b
Maxim Biloborodko <berzerker994@gmail.com>
2021-11-24 18:17:10 +0200

    2.0

--- sysdir/5info/info.py
+++ sysdir/5info/info.py
@@ -3,7 +3,33 @@ import pyodbc
 import re
 import subprocess
 
+def execresult(result):
+      try:
+          inforesult = result[0]
+      except:
+          print("\n\n\t||============================================||"
+                "\n\t||  НЕ ЗНАЙДЕНО ІНФО В БД ІЗ ТАКИМИ ДАНИМИ "
+                "\n\t||============================================||"
+                "\n")
+      else:
+          infores = str(inforesult).lstrip("(").rstrip(")").split(", ")
+          print("\n\n\t||============================================||"
+                "\n\t||  МОДЕЛЬ ТЕЛЕФОНУ - " + str(infores[2]).strip("'") + " "
+                "\n\t||  МАС-АДРЕСА - " + str(infores[0]).strip("'") + " "
+                "\n\t||  IНВЕНТАРНИЙ НОМЕР - " + str(infores[4]).strip("'") + " "
+                "\n\t||  IP-АДРЕСА - " + str(infores[1]).strip("'") + " "
+                "\n\t||  ПРИСВОЄНИЙ НОМЕР - " + str(infores[3]).strip("'") + " "
+                "\n\t||  ЛОКАЦIЯ - " + str(infores[5]).strip("'") + " "
+                "\n\t||============================================||"
+                "\n")
 
+def sqlconn(query):
+      connect = pyodbc.connect('DRIVER={SQL Server}; SERVER=10.0.0.9; PORT=1433; DATABASE=IpTele; UID=sa; PWD=123456;')
+      cursor = connect.cursor()
+      cursor.execute(query)
+      result = cursor.fetchall()
+      connect.close()
+      execresult(result)
 
 print("\n\n\t||============================================||"
       "\n\t||  ЩОБ ОТРИМАТИ IНФО ПРО ПРИСТРIЙ            ||"
@@ -25,118 +51,24 @@ resm = re.match(macq, str(quer))
 resip = re.match(ipq, str(quer))
 # 4================================
 resinv = re.match(invq, str(quer))
+
 # умова + sql-query==================================
 if (resm != None):
       resultmac = str(resm[0])
-      connect = pyodbc.connect('DRIVER={SQL Server}; SERVER=10.0.0.9; PORT=1433; DATABASE=IpTele; UID=sa; PWD=123456;')
-      cursor = connect.cursor()
-      query = ("SELECT * FROM [dbo].[IpTelephony] WHERE [MacAddress] = '{ma}'".format(ma = resultmac))
-      cursor.execute(query)
-      result = cursor.fetchall()
-      connect.close()
-      try:
-          inforesult = result[0]
-      except:
-          print("\n\n\t||============================================||"
-                "\n\t||  НЕ ЗНАЙДЕНО ІНФО В БД ІЗ ТАКИМИ ДАНИМИ "
-                "\n\t||============================================||"
-                "\n")
-      else:
-          infores = str(inforesult).lstrip("(").rstrip(")").split(", ")
-          print("\n\n\t||============================================||"
-                "\n\t||  МОДЕЛЬ ТЕЛЕФОНУ - " + str(infores[2]).strip("'") + " "
-                "\n\t||  МАС-АДРЕСА - " + str(infores[0]).strip("'") + " "
-                "\n\t||  IНВЕНТАРНИЙ НОМЕР - " + str(infores[4]).strip("'") + " "
-                "\n\t||  IP-АДРЕСА - " + str(infores[1]).strip("'") + " "
-                "\n\t||  ПРИСВОЄНИЙ НОМЕР - " + str(infores[3]).strip("'") + " "
-                "\n\t||  ЛОКАЦIЯ - " + str(infores[5]).strip("'") + " "
-                "\n\t||============================================||"
-                "\n")
-
-
+      query = ("SELECT * FROM [dbo].[IpTelephony] WHERE [MacAddress] = '{ma}'".format(ma=resultmac))
+      sqlconn(query)
 elif ((resm == None) and (resinv != None)):
       resultinv = str(resinv[0])
-      connect = pyodbc.connect('DRIVER={SQL Server}; SERVER=10.0.0.9; PORT=1433; DATABASE=IpTele; UID=sa; PWD=123456;')
-      cursor = connect.cursor()
-      query = ("SELECT * FROM [dbo].[IpTelephony] WHERE [InvNumber] = '{invn}'".format(invn = resultinv))
-      cursor.execute(query)
-      result = cursor.fetchall()
-      connect.close()
-      try:
-          inforesult = result[0]
-      except:
-          print("\n\n\t||============================================||"
-                "\n\t||  НЕ ЗНАЙДЕНО ІНФО В БД ІЗ ТАКИМИ ДАНИМИ "
-                "\n\t||============================================||"
-                "\n")
-      else:
-          infores = str(inforesult).lstrip("(").rstrip(")").split(", ")
-          print("\n\n\t||============================================||"
-                "\n\t||  МОДЕЛЬ ТЕЛЕФОНУ - " + str(infores[2]).strip("'") + " "
-                "\n\t||  МАС-АДРЕСА - " + str(infores[0]).strip("'") + " "
-                "\n\t||  IНВЕНТАРНИЙ НОМЕР - " + str(infores[4]).strip("'") + " "
-                "\n\t||  IP-АДРЕСА - " + str(infores[1]).strip("'") + " "
-                "\n\t||  ПРИСВОЄНИЙ НОМЕР - " + str(infores[3]).strip("'") + " "
-                "\n\t||  ЛОКАЦIЯ - " + str(infores[5]).strip("'") + " "
-                "\n\t||============================================||"
-                "\n")
-
-
-
+      query = ("SELECT * FROM [dbo].[IpTelephony] WHERE [InvNumber] = '{invn}'".format(invn=resultinv))
+      sqlconn(query)
 elif ((resm == None) and (resinv == None) and (resn != None)):
       resultnum = str(resn[0])
-      connect = pyodbc.connect('DRIVER={SQL Server}; SERVER=10.0.0.9; PORT=1433; DATABASE=IpTele; UID=sa; PWD=123456;')
-      cursor = connect.cursor()
-      query = ("SELECT * FROM [dbo].[IpTelephony] WHERE [PhoneNumber] = '{phn}'".format(phn = resultnum))
-      cursor.execute(query)
-      result = cursor.fetchall()
-      connect.close()
-      try:
-          inforesult = result[0]
-      except:
-          print("\n\n\t||============================================||"
-                "\n\t||  НЕ ЗНАЙДЕНО ІНФО В БД ІЗ ТАКИМИ ДАНИМИ "
-                "\n\t||============================================||"
-                "\n")
-      else:
-          infores = str(inforesult).lstrip("(").rstrip(")").split(", ")
-          print("\n\n\t||============================================||"
-                "\n\t||  МОДЕЛЬ ТЕЛЕФОНУ - " + str(infores[2]).strip("'") + " "
-                "\n\t||  МАС-АДРЕСА - " + str(infores[0]).strip("'") + " "
-                "\n\t||  IНВЕНТАРНИЙ НОМЕР - " + str(infores[4]).strip("'") + " "
-                "\n\t||  IP-АДРЕСА - " + str(infores[1]).strip("'") + " "
-                "\n\t||  ПРИСВОЄНИЙ НОМЕР - " + str(infores[3]).strip("'") + " "
-                "\n\t||  ЛОКАЦIЯ - " + str(infores[5]).strip("'") + " "
-                "\n\t||============================================||"
-                "\n")
-
-
+      query = ("SELECT * FROM [dbo].[IpTelephony] WHERE [PhoneNumber] = '{phn}'".format(phn=resultnum))
+      sqlconn(query)
 else:
       resultip = str(resip[0])
-      connect = pyodbc.connect('DRIVER={SQL Server}; SERVER=10.0.0.9; PORT=1433; DATABASE=IpTele; UID=sa; PWD=123456;')
-      cursor = connect.cursor()
-      query = ("SELECT * FROM [dbo].[IpTelephony] WHERE [IpAddress] = '{ipa}'".format(ipa = resultip))
-      cursor.execute(query)
-      result = cursor.fetchall()
-      connect.close()
-      try:
-          inforesult = result[0]
-      except:
-          print("\n\n\t||============================================||"
-                "\n\t||  НЕ ЗНАЙДЕНО ІНФО В БД ІЗ ТАКИМИ ДАНИМИ "
-                "\n\t||============================================||"
-                "\n")
-      else:
-          infores = str(inforesult).lstrip("(").rstrip(")").split(", ")
-          print("\n\n\t||============================================||"
-                "\n\t||  МОДЕЛЬ ТЕЛЕФОНУ - " + str(infores[2]).strip("'") + " "
-                "\n\t||  МАС-АДРЕСА - " + str(infores[0]).strip("'") + " "
-                "\n\t||  IНВЕНТАРНИЙ НОМЕР - " + str(infores[4]).strip("'") + " "
-                "\n\t||  IP-АДРЕСА - " + str(infores[1]).strip("'") + " "
-                "\n\t||  ПРИСВОЄНИЙ НОМЕР - " + str(infores[3]).strip("'") + " "
-                "\n\t||  ЛОКАЦIЯ - " + str(infores[5]).strip("'") + " "
-                "\n\t||============================================||"
-                "\n")
+      query = ("SELECT * FROM [dbo].[IpTelephony] WHERE [IpAddress] = '{ipa}'".format(ipa=resultip))
+      sqlconn(query)
 
 while True:
       endinfo = input("Натисніть 1, щоб вийти з програми або 2, щоб повернутись до головного меню..")
