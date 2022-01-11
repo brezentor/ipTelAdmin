5945ea28d20c0fc54f73668da9ebedd6b9b0b95b
Max Biloborodko <90316251+brezentor@users.noreply.github.com>
2022-01-11 22:48:24 +0200

    Update hard.ps1

--- sysdir/hard.ps1
+++ sysdir/hard.ps1
@@ -1,4 +1,4 @@
-$rez = Get-DhcpServerv4Reservation -ComputerName "ad1.krayina.local" -ScopeId "10.30.0.0" | sort IpAddress | Format-Table -Property IpAddress -auto -HideTableHeaders | Out-String
+$rez = Get-DhcpServerv4Reservation -ComputerName "name.domain.local" -ScopeId "10.40.0.0" | sort IpAddress | Format-Table -Property IpAddress -auto -HideTableHeaders | Out-String
 
 #=3==============\внесение всех резервированных  ip в reserved.log\=========================
 
