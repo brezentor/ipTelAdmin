43b597129aa1da72efaa4bc7e51f16b0351f2717
Max Biloborodko <90316251+brezentor@users.noreply.github.com>
2022-01-11 22:46:36 +0200

    Update delreserv.ps1

--- sysdir/delreserv.ps1
+++ sysdir/delreserv.ps1
@@ -3,7 +3,7 @@ $delfals = Get-Content -Path "C:\scripts\ipTelAdmin\logdir\dlfls.tmp"
 $d = $delfals.Split(",")
 
 foreach ($el in $d) {
-  Remove-DhcpServerv4Reservation -ComputerName "ad1.krayina.local" -IPAddress $el
+  Remove-DhcpServerv4Reservation -ComputerName "name.domain.local" -IPAddress $el
 }
 
 Remove-Item -Path "C:\scripts\ipTelAdmin\logdir\dlfls.tmp"
